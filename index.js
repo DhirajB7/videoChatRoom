0caeb3714a0b667cec2ba59bd39f586522751811
Dhiraj <bond.dhiraj@yahoo.co.in>
2021-04-14 15:21:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import {v4} from 'uuid'
+
+// server details
+const app = express();
+const PORT = 4000;
+const httpServer = createServer(app);
+const options = {
+  cors: {
+    origin: "http://localhost:4000",
+  },
+};
+
+//views
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//end-points
+app.get("/check", (req, res) => res.json({ status: "working" }));
+
+app.get("/", (req,res) => {
+    res.redirect(`/${v4()}`)
+})
+
+
+app.get("/:room", (req,res) => {
+    res.render('room',{roomId:req.params.room})
+})
+
+httpServer.listen(PORT, () => console.log(`started at port ${PORT}`));
+
+//socket
+const io = new Server(httpServer, options);
+
+io.on('connection',socket => {
+    socket.on("join",(roomId,userId) => {
+        
+        socket.join(roomId)
+
+        socket.broadcast.to(roomId).emit("user-connected",userId)
+
+        socket.on('disconnect',()=>{
+            socket.broadcast.to(roomId).emit("user-disconnected",userId)
+        })
+    })
+})
+
---
+++ public/script.js
@@ -0,0 +1,69 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "4001",
+});
+
+const peers = {};
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    myPeer.on("call", (call) => {
+      call.answer(stream); // 2ed user  to 1st user screen
+
+      //1st user to 2ed user
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  });
+
+myPeer.on("open", (id) => {
+  socket.emit("join", ROOM_ID, id);
+});
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) {
+    peers[userId].close();
+  }
+});
+
+//add call to video grid
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
+
+// make call when new users join
+const connectToNewUser = (userId, stream) => {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+
+  call.on("close", () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+};
